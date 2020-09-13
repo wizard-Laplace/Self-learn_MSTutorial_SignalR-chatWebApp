c3cec6d131eeb8cde3f12a081b3223e210f38630
wizard-laplace <54266481+wizard-Laplace@users.noreply.github.com>
2020-09-13 13:21:57 +0900

    タイポ修正

--- wwwroot/js/chat.js
+++ wwwroot/js/chat.js
@@ -7,23 +7,23 @@ document.getElementById("sendButton").disabled = true;
 
 connection.on("ReceiveMessage", function (user, message) {
     var msg = message.replace(/&/g, "&amp;").replace(/</g, "&lt;").replace(/>/g, "&gt;");
-    var encodedMsg = user + "says " + msg;
+    var encodedMsg = user + " says " + msg;
     var li = document.createElement("li");
     li.textContent = encodedMsg;
-    document.getElementById("messageList").appendChild(li);
+    document.getElementById("messagesList").appendChild(li);
 });
 
 connection.start().then(function () {
     document.getElementById("sendButton").disabled = false;
 }).catch(function (err) {
-    return console.error(err.tostring());
+    return console.error(err.toString());
 });
 
 document.getElementById("sendButton").addEventListener("click", function (event) {
     var user = document.getElementById("userInput").value;
     var message = document.getElementById("messageInput").value;
     connection.invoke("SendMessage", user, message).catch(function (err) {
-        return console.error(err.tostring());
+        return console.error(err.toString());
     });
     event.preventDefault();
 });
